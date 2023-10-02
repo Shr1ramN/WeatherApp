550a932b0fceca92c38835c786d098af6491da88
Shriram Narayana <shriramnarayanabhat@gmail.com>
2023-10-02 20:36:17 +0530

    updated

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getWeatherData(cityname: string) {
+    this.http.get<weatherData>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+        .set(
+          environment.XRapidAPIHostHeaderName,
+          environment.XRapidAPIHostHeaderValue
+        )
+        .set(environment.XRapidAPIKey, environment.XRapidAPIKeyValue),
+      params: new HttpParams()
+        .set('q', cityname)
+        .set('units', 'metric')
+        .set('mode', 'json'),
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKey: 'X-RapidAPI-Key',
+    XRapidAPIKeyValue: '5fe5f7a209msh10a532303e2948ap17ebfdjsnb57747f18920'
+};
